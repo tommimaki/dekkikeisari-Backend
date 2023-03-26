0bbb0abe0b3099498a8d27a15c94219d63e37965
tommibish <74586216+tommimaki@users.noreply.github.com>
2023-03-26 20:13:51 +0300

    sql database setup

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const db = require("./db");
 
 dotenv.config();
 
@@ -10,6 +11,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+db.query("SELECT 1 + 1 AS solution", (err, results) => {
+  if (err) {
+    console.error("Error executing query:", err.stack);
+  } else {
+    console.log("Query result:", results[0].solution);
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("Hello from the backend!");
