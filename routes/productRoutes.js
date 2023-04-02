d345f742db800aa1b8c078fe26c295c35d58739a
tommibish <74586216+tommimaki@users.noreply.github.com>
2023-04-02 10:30:11 +0300

    removed commented unused code

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,24 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const multer = require("multer");
-// const upload = require("../uploadConfig");
-
-// const {
-//   addProduct,
-//   getAllProducts,
-//   getProductById,
-// } = require("../controllers/productController");
-// // const productController = require("../controllers/productController");
-
-// // ADD PRODUCTS
-// // router.post("/add", addProduct);
-// router.post("/add", upload.single("image"), addProduct);
-
-// router.get("/", getAllProducts);
-// router.get("/:id", getProductById);
-
-// module.exports = router;
-
 const express = require("express");
 const router = express.Router();
 const { upload, processImage } = require("../uploadConfig");
