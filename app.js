9f685f5b20b3009ed6289560fd603cdc5b547053
tommibish <74586216+tommimaki@users.noreply.github.com>
2023-04-04 21:30:38 +0300

    added ratelimiter

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const rateLimit = require("express-rate-limit");
+
+//routes
 const productRoute = require("./routes/productRoutes");
 const orderRoute = require("./routes/orderRoutes");
-const { pool } = require("./DB/db");
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/authRoutes");
+
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
 app.get("/", (req, res) => {
-  res.send("Hello from the backend!");
+  res.send("Backend for a portfolio project for a skateboarding online store");
 });
 
-app.get("/testdb", async (req, res) => {
-  try {
-    const [rows] = await pool.query("SELECT 1+1 AS solution");
-    res.status(200).json({ message: "DB Connection successful!", data: rows });
-  } catch (error) {
-    console.error("Error in DB connection:", error); // Add this line to log the error
-    res.status(500).json({ message: "DB Connection failed!", error });
-  }
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 30, // limit each IP to 30 requests per 15 min// dont overload my aws LOL
+  message: "Too many requests from this IP, please try again later",
 });
+
 // Pass the upload instance to the productRoute
 app.use("/products", productRoute);
 app.use("/orders", orderRoute);
 app.use("/users", userRoutes);
 app.use("/auth", authRoutes);
+
+app.use(cors());
+app.use(express.json());
+app.use(limiter);
 
 module.exports = app;
