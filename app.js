3f0d1e4494dddc355238118c99100e103a3604dd
tommibish <74586216+tommimaki@users.noreply.github.com>
2023-04-05 17:27:36 +0300

    before modifying to multiple pictures per product

--- app.js
+++ app.js
@@ -12,25 +12,24 @@ const authRoutes = require("./routes/authRoutes");
 dotenv.config();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Backend for a portfolio project for a skateboarding online store");
 });
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 30, // limit each IP to 30 requests per 15 min// dont overload my aws LOL
-  message: "Too many requests from this IP, please try again later",
-});
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 30, // limit each IP to 30 requests per 15 min// dont overload my aws LOL
+//   message: "Too many requests from this IP, please try again later",
+// });
+// app.use(limiter);
 
 // Pass the upload instance to the productRoute
 app.use("/products", productRoute);
 app.use("/orders", orderRoute);
 app.use("/users", userRoutes);
 app.use("/auth", authRoutes);
-
-app.use(cors());
-app.use(express.json());
-app.use(limiter);
 
 module.exports = app;
